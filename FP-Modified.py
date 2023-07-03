c2f03a03820446d4d91563566566992ad8b24314
Gusnand <72637679+Gusnand@users.noreply.github.com>
2023-07-03 20:13:18 +0800

    Update FP-Modified.py

--- FP-Modified.py
+++ FP-Modified.py
@@ -21,13 +21,12 @@ def create_model(input_shape, num_classes, num_hidden_layers, num_hidden_neurons
     """
     model = Sequential()
     model.add(Dense(num_hidden_neurons, activation=activation, input_shape=input_shape))
-    
+
     for _ in range(num_hidden_layers - 1):
         model.add(Dense(num_hidden_neurons, activation=activation))
-    
+
     model.add(Dense(num_classes, activation='softmax'))
     return model
-
 
 def train_model(model, X_train, y_train, lr, num_epochs):
     """
@@ -38,21 +37,19 @@ def train_model(model, X_train, y_train, lr, num_epochs):
     model.fit(X_train, y_train, epochs=num_epochs, batch_size=32, verbose=0)
     return model
 
-
 def evaluate_model(model, X_test, y_test):
     """
     Mengevaluasi model menggunakan data test dan menghitung metrik evaluasi seperti akurasi, presisi, recall, dan F1-Score.
     """
     y_pred = np.argmax(model.predict(X_test), axis=1)
     y_test = np.argmax(y_test, axis=1)
-    
+
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
-    
-    return accuracy, precision, recall, f1
 
+    return accuracy, precision, recall, f1
 
 def preprocess_audio(audio_path, label):
     """
@@ -63,7 +60,6 @@ def preprocess_audio(audio_path, label):
     mfccs_mean = np.mean(mfccs, axis=1)
     return mfccs_mean, label
 
-
 def load_data(path):
     """
     Memuat data audio dari file .wav sesuai dengan path yang diberikan.
@@ -71,18 +67,17 @@ def load_data(path):
     audio_files = glob.glob(path)
     X = []
     y = []
-    
+
     for audio_file in audio_files:
         label = 0 if 'happy' in audio_file else 1
         mfccs_mean, label = preprocess_audio(audio_file, label)
         X.append(mfccs_mean)
         y.append(label)
-        
+
     X = np.array(X)
     y = np.array(y)
-    
-    return X, y
 
+    return X, y
 
 def deploy_web_app(best_model):
     """
@@ -90,9 +85,9 @@ def deploy_web_app(best_model):
     """
     st.title("Identifikasi Sentimen dari Suara")
     st.write("Aplikasi ini menggunakan model neural network untuk mengidentifikasi sentimen atau emosi dari suara.")
-    
+
     audio_files = st.file_uploader("Upload satu atau beberapa file audio", type=".wav", accept_multiple_files=True)
-    
+
     if audio_files is not None:
         for audio_file in audio_files:
             signal, sample_rate = librosa.load(audio_file)
@@ -100,19 +95,18 @@ def deploy_web_app(best_model):
             mfccs_mean = np.mean(mfccs, axis=1)
             input_data = np.array([mfccs_mean])
             sentiment = "Positive" if np.argmax(best_model.predict(input_data)) == 0 else "Negative"
-            
+
             st.write("Hasil identifikasi sentimen:")
             st.write(f"File: {audio_file.name}")
             st.write(f"Sentimen: {sentiment}")
             st.write("=========================================")
-
 
 def main(path):
     """
     Fungsi utama yang menjalankan alur aplikasi.
     """
     X, y = load_data(path)
-    
+
     num_classes = 2  # Jumlah kelas (positive sentiment dan negative sentiment)
     input_shape = (13,)  # Bentuk data input (fitur MFCC)
 
@@ -124,38 +118,38 @@ def main(path):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
         y_train = to_categorical(y_train, num_classes)
         y_test = to_categorical(y_test, num_classes)
-    
+
+        learning_rates = [0.01, 0.001, 0.0001]
+        num_epochs_list = [50, 100, 200]
+        hidden_layers_list = [1, 2, 3]
+        hidden_neurons_list = [32, 64, 128]
+        activations = ['sigmoid', 'softmax', 'tanh']
+
+        best_accuracy = 0
+        best_model = None
+
+        for lr in learning_rates:
+            for num_epochs in num_epochs_list:
+                for num_hidden_layers in hidden_layers_list:
+                    for num_hidden_neurons in hidden_neurons_list:
+                        for activation in activations:
+                            model = create_model(input_shape, num_classes, num_hidden_layers, num_hidden_neurons, activation)
+                            model = train_model(model, X_train, y_train, lr, num_epochs)
+                            accuracy, precision, recall, f1 = evaluate_model(model, X_test, y_test)
+
+                            if accuracy > best_accuracy:
+                                best_accuracy = accuracy
+                                best_model = model
+
+                            print(f"Learning Rate: {lr}, Epochs: {num_epochs}, Hidden Layers: {num_hidden_layers}, Hidden Neurons: {num_hidden_neurons}, Activation: {activation}")
+                            print(f"Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-Score: {f1}")
+                            print("=========================================")
+
+        deploy_web_app(best_model)
+
     else:
         print("Error: Data tidak cukup untuk dibagi menjadi data training dan data testing.")
-    
-    learning_rates = [0.01, 0.001, 0.0001]
-    num_epochs_list = [50, 100, 200]
-    hidden_layers_list = [1, 2, 3]
-    hidden_neurons_list = [32, 64, 128]
-    activations = ['sigmoid', 'softmax', 'tanh']
-    
-    best_accuracy = 0
-    best_model = None
-    
-    for lr in learning_rates:
-        for num_epochs in num_epochs_list:
-            for num_hidden_layers in hidden_layers_list:
-                for num_hidden_neurons in hidden_neurons_list:
-                    for activation in activations:
-                        model = create_model(input_shape, num_classes, num_hidden_layers, num_hidden_neurons, activation)
-                        model = train_model(model, X_train, y_train, lr, num_epochs)
-                        accuracy, precision, recall, f1 = evaluate_model(model, X_test, y_test)
-                        
-                        if accuracy > best_accuracy:
-                            best_accuracy = accuracy
-                            best_model = model
-                        
-                        print(f"Learning Rate: {lr}, Epochs: {num_epochs}, Hidden Layers: {num_hidden_layers}, Hidden Neurons: {num_hidden_neurons}, Activation: {activation}")
-                        print(f"Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-Score: {f1}")
-                        print("=========================================")
-    
-    deploy_web_app(best_model)
-
 
 # Contoh penggunaan
 main('FP-AudioJST/audiosamples/*.wav')
+
